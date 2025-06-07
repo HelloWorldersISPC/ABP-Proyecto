1c029b62ce2b341f106bb079ac927517fb2fcfd1
cholobackcod <choloman14@gmail.com>
2025-06-07 20:02:37 -0300

    se corrige mi error

--- menu.py
+++ menu.py
@@ -21,7 +21,6 @@ usuarios = [{
         "rol": "administrador",
     }]
 dispositivos = []
-usuario_acual= []
 
 
 def menu_administrador(dispositivos, escenarios, rol_actual="admin"):
@@ -79,7 +78,7 @@ def menu_invitado(dispositivos, escenarios, rol_actual="invitado"):
         elif opcion == '3':
             usar_escenario(escenarios, dispositivos)
         elif opcion == '4':
-            continue
+            ver_mi_perfil(usuario_actual)
         elif opcion == '0':
             print("Sesión cerrada.")
             break
